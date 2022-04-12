cb6196b284ccea66fb1e138a9910034c01045ee7
Nguyen Nam Duong <DuongNNb@bkav.com>
2022-04-12 09:00:48 +0700

    Commit mồi.

--- src/main/java/com/example/Todolist/Repository/TodolistRepository.java
+++ src/main/java/com/example/Todolist/Repository/TodolistRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+//
 @Repository
 public interface TodolistRepository extends MongoRepository<Todolist, String> {
     Todolist save(Todolist job);
